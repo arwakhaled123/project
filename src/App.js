c2b968496c1dc8e630ef26e58cfd12fac106ec83
arwakhaled123 <arwa1348khaled@gmail.com>
2024-12-02 00:11:31 +0200

    done2

--- src/App.js
+++ src/App.js
@@ -2,25 +2,43 @@
 import { Fragment } from 'react';
 import './App.css';
 import Navbars from './Components/Nav/Navs';
-import {LoginPage} from './Components/login/login';
+// import {LoginPage} from './Components/login/login';
 import { SignUps } from './Components/register/SignUp';
+// import { LoginPage } from './Components/login/login';
+
 import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
+import LoginPage from './Components/login/login'; // Default import
 
 function App() {
   return (
-    <App/>
-
-    // another way to connect pages
+    <SignUps/>
+    // For routing, uncomment and configure below
     // <Router>
     //   <Switch>
-    //     <Route exact Component={LoginPage} path='/login'/>
-    //     <Route exact Component={SignUps} path='/SignUp'/>
-
+    //     <Route exact component={LoginPage} path='/login'/>
+    //     <Route exact component={SignUps} path='/SignUp'/>
     //   </Switch>
     // </Router>
-  
   );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <LoginPage />
+
+//     // another way to connect pages
+//     // <Router>
+//     //   <Switch>
+//     //     <Route exact Component={LoginPage} path='/login'/>
+//     //     <Route exact Component={SignUps} path='/SignUp'/>
+
+//     //   </Switch>
+//     // </Router>
+  
+//   );
+// }
+
+// export default App;
